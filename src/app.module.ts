ffd888fa14ed57f350d73a539c75e4d46c71ff2c
rodrigo15975 <javierluis15975@hotmail.com>
2025-04-19 05:35:26 -0500

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,33 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq'
-import { ConfigModule } from '@nestjs/config'
-import { UserModule } from './module/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { UserModule } from './module/user/user.module'
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    RabbitMQModule.forRoot({
-      exchanges: [
-        {
-          name: 'user-exchange',
-          type: 'direct',
+    RabbitMQModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        exchanges: [
+          {
+            name: 'user-exchange',
+            type: 'direct',
+          },
+        ],
+        queues: [
+          {
+            name: 'user-queue',
+          },
+        ],
+        uri: configService.getOrThrow<string>('RABBITMQ_URL'),
+        connectionInitOptions: {
+          wait: true,
+          timeout: 5000,
         },
-      ],
-      queues: [
-        {
-          name: 'user-queue',
-        },
-      ],
-      uri: 'amqps://zdktidoi:ZV6BpAaqMPbJ2VBqISAgT8uT-rtrWZph@jaragua.lmq.cloudamqp.com/zdktidoi',
-      connectionInitOptions: {
-        wait: true,
-        timeout: 5000,
-      },
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
   ],
